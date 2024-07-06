eb91f44b3f9485e7f9956d2d9a0efcba27f963bb
Akshith <garapatiakshith@gmail.com>
2024-07-06 13:57:26 +0530

    overhaul window creation + fix overlay ui

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,17 +3,39 @@
     windows_subsystem = "windows"
 )]
 
+#[macro_use]
+extern crate objc;
+
+mod window_custom;
+
 use std::fs;
 use std::path::Path;
 use std::sync::Arc;
-use tauri::{GlobalShortcutManager, Manager, Window};
 
+use tauri::{GlobalShortcutManager, Manager};
+// use tauri_nspanel::WebViewWindowExt;
 
+#[cfg(target_os = "macos")]
+use tauri::Window;
+
+// use window_custom::WindowExt;
+
+#[cfg(target_os = "macos")]
+use window_custom::macos::WindowExtMacos;
+
+#[cfg(target_os = "macos")]
+fn apply_macos_specifics(window: &Window) {
+    window.set_visisble_on_all_workspaces(true);
+    window.set_transparent_titlebar(true, true);
+}
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let app_handle = Arc::new(app.handle());
+            let overlay = app
+                .get_window("overlay")
+                .ok_or("Overlay window not found")?;
 
             // Register global shortcuts
             let app_handle_clone = Arc::clone(&app_handle);
@@ -26,6 +48,10 @@ fn main() {
                 Ok(_) => println!("CommandOrControl+Shift+C registered successfully"),
                 Err(e) => println!("Failed to register CommandOrControl+Shift+C: {}", e),
             };
+
+            // add mac things
+            #[cfg(target_os = "macos")]
+            apply_macos_specifics(&overlay);
 
             Ok(())
         })
@@ -78,6 +104,19 @@ fn hide_search_bar(window: Window) -> Result<(), String> {
 }
 
 #[tauri::command]
+fn show_overlay(window: Window) -> Result<(), String> {
+    if let Some(overlay) = window.get_window("overlay") {
+        overlay.show().map_err(|e| e.to_string())?;
+        overlay.set_focus().map_err(|e| e.to_string())?;
+        window
+            .app_handle()
+            .emit_all("overlay-shown", ())
+            .map_err(|e| e.to_string())?;
+    }
+    Ok(())
+}
+
+#[tauri::command]
 fn check_and_create_file(directory: String) -> Result<(), String> {
     let file_path = Path::new(&directory).join("master-ingest-data.json");
 
@@ -88,18 +127,5 @@ fn check_and_create_file(directory: String) -> Result<(), String> {
         println!("File already exists: {:?}", file_path);
     }
 
-    Ok(())
-}
-
-#[tauri::command]
-fn show_overlay(window: Window) -> Result<(), String> {
-    if let Some(overlay) = window.get_window("overlay") {
-        overlay.show().map_err(|e| e.to_string())?;
-        overlay.set_focus().map_err(|e| e.to_string())?;
-        window
-            .app_handle()
-            .emit_all("overlay-shown", ())
-            .map_err(|e| e.to_string())?;
-    }
     Ok(())
 }
---
+++ src-tauri/src/window_custom.rs
@@ -0,0 +1,92 @@
+#[cfg(target_os = "macos")]
+
+pub mod macos {
+    use cocoa::{
+        appkit::{
+            NSMainMenuWindowLevel, NSWindow, NSWindowButton, NSWindowCollectionBehavior,
+            NSWindowStyleMask, NSWindowTitleVisibility,
+        },
+        base::id,
+        foundation::NSInteger,
+    };
+    use objc::{msg_send, runtime::YES};
+    use tauri::{Runtime, Window};
+    pub trait WindowExtMacos {
+        fn set_transparent_titlebar(&self, title_transparent: bool, remove_toolbar: bool);
+        fn set_visisble_on_all_workspaces(&self, enabled: bool);
+    }
+
+    impl<R: Runtime> WindowExtMacos for Window<R> {
+        fn set_visisble_on_all_workspaces(&self, enabled: bool) {
+            const HIGHER_LEVEL_THAN_LEAGUE: NSInteger = 1001;
+            unsafe {
+                let ns_win = self.ns_window().unwrap() as id;
+
+                if enabled {
+                    ns_win.setLevel_(HIGHER_LEVEL_THAN_LEAGUE);
+                    ns_win.setCollectionBehavior_(
+                        NSWindowCollectionBehavior::NSWindowCollectionBehaviorCanJoinAllSpaces,
+                    );
+                } else {
+                    ns_win.setLevel_(((NSMainMenuWindowLevel - 1) as u64).try_into().unwrap());
+                    ns_win.setCollectionBehavior_(
+                        NSWindowCollectionBehavior::NSWindowCollectionBehaviorDefault,
+                    );
+                }
+            }
+        }
+
+        fn set_transparent_titlebar(&self, title_transparent: bool, remove_tool_bar: bool) {
+            unsafe {
+                let id = self.ns_window().unwrap() as cocoa::base::id;
+                NSWindow::setTitlebarAppearsTransparent_(id, cocoa::base::YES);
+                let mut style_mask = id.styleMask();
+                style_mask.set(
+                    NSWindowStyleMask::NSFullSizeContentViewWindowMask,
+                    title_transparent,
+                );
+                id.setStyleMask_(style_mask);
+                if remove_tool_bar {
+                    let close_button =
+                        id.standardWindowButton_(NSWindowButton::NSWindowCloseButton);
+                    let _: () = msg_send![close_button, setHidden: YES];
+                    let min_button =
+                        id.standardWindowButton_(NSWindowButton::NSWindowMiniaturizeButton);
+                    let _: () = msg_send![min_button, setHidden: YES];
+                    let zoom_button = id.standardWindowButton_(NSWindowButton::NSWindowZoomButton);
+                    let _: () = msg_send![zoom_button, setHidden: YES];
+                }
+                id.setTitleVisibility_(if title_transparent {
+                    NSWindowTitleVisibility::NSWindowTitleHidden
+                } else {
+                    NSWindowTitleVisibility::NSWindowTitleVisible
+                });
+
+                #[cfg(target_arch = "aarch64")]
+                id.setHasShadow_(false);
+
+                #[cfg(target_arch = "x86_64")]
+                id.setHasShadow_(0);
+
+                id.setTitlebarAppearsTransparent_(if title_transparent {
+                    cocoa::base::YES
+                } else {
+                    cocoa::base::NO
+                });
+            }
+        }
+    }
+}
+
+use tauri::{Runtime, Window};
+
+pub trait WindowExt {
+    fn set_document_title(&self, url: &str);
+}
+
+impl<R: Runtime> WindowExt for Window<R> {
+    fn set_document_title(&self, title: &str) {
+        let str = format!("document.title = '{:}'", title);
+        self.eval(&str).unwrap();
+    }
+}
